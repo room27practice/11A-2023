670278692f95c78a2c8cc99b03774255094dc9f4
a <b>
2023-09-20 12:30:02 +0300

    Added Properties And Fields Demo

---
+++ OOP/OOP 11A/DefiniraneNaPoletaIsvoistva/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace DefiniraneNaPoletaIsvoistva
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+            Dice d1 = new Dice();
+            Dice d2 = new Dice();
+            d1.Sides = 7;
+            d2.Sides = 5;
+            d1.PresentInfo();
+            //  Console.WriteLine($"Broiat strani na zar1 e: {d1.Sides}. A cveta na zarcheto e {d1.Color}");
+            Console.WriteLine(new string('=', 20));
+            d1.Color = "Magenta";
+            d1.Sides = 15;       
+
+            d1.PresentInfo();
+            for (int i = 0; i < 10; i++)
+            {
+                int rolledNumber = d1.RollDice();
+                Console.WriteLine($"Hvurlihme zarche N1 i poluchihme:{rolledNumber}");
+                Thread.Sleep(1000);
+            }
+
+            d2.PresentInfo();
+            for (int i = 0; i < 10; i++)
+            {
+                int rolledNumber = d2.RollDice();
+                Console.WriteLine($"Hvurlihme zarche N1 i poluchihme:{rolledNumber}");
+                Thread.Sleep(1000);
+            }
+
+
+
+            Console.WriteLine("Broiat strani na zar2 e: " + d2.Sides);
+
+            Dice d3 = new Dice { Sides=12, Color = "Oranjevo" };
+
+            d3.PresentInfo();
+
+        }
+    }
+
+    public class Dice
+    {
+        int sides;
+        public int Sides { get => sides; set => sides = value; }
+
+        string color = "white";
+        public string Color { get => color; set => color = value; }
+
+        public void PresentInfo()
+        {
+            Console.WriteLine($"Broiat strani na zar1 e: {Sides}. A cveta na zarcheto e {Color}");
+        }
+
+        public int RollDice()
+        {
+            Random rnd = new Random();
+            int result = rnd.Next(1, Sides+1);
+            return result;
+        }
+
+
+    }
+
+
+
+
+
+
+
+}
---
+++ OOP/OOP 11A/ZadachiZaPregovor/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace ZadachiZaPregovor
+{
+    internal class StartUP
+    {
+        static void Main()
+        {
+            //  Zadacha1();
+             Zadacha2();
+
+
+        }
+
+        private static void Zadacha1()
+        {
+            int rentOfHall = int.Parse(Console.ReadLine());
+            decimal statuesValue = rentOfHall * 0.7m;
+            decimal ceteringValue = statuesValue * 0.85m;
+            var soundValue = ceteringValue / 2;
+            decimal totalCost = rentOfHall + statuesValue + ceteringValue + soundValue;
+            Console.WriteLine($"Otgovorut e {totalCost:F2}");
+        }
+
+
+        private static void Zadacha2()
+        {
+            Console.WriteLine("Tova e zadacha 2!!!!!!!");
+        }
+    }
+}
