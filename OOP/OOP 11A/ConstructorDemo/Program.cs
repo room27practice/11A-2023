f49c23ed80c29010ff6a8db286632f840b179fa3
a <b>
2023-10-02 11:41:36 +0300

    add

---
+++ OOP/OOP 11A/ConstructorDemo/Program.cs
@@ -0,0 +1,117 @@
+﻿using System.Globalization;
+
+namespace ConstructorDemo
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            #region SpisuciPrimer
+            //var listNums = new List<int>();
+
+            //listNums.Add(1);
+            //listNums.Add(5);
+            //listNums.Add(3);
+
+
+            //var listNums2 = new List<int>() { 1,5,3};
+            //listNums2.Add(31);
+            #endregion
+
+
+            var man = new Manufacturer();
+            man.Name = "Georgi";
+            var man2 = new Manufacturer("Atanas","+359-953-67-23");
+
+            var man3 = new Manufacturer("Hristiqn","+359888753","vuzdoh.com","hristiqn@abv.bg");
+
+            var proizvoditeli = new List<Manufacturer>()
+            {
+                new Manufacturer()
+                    {
+                    Id = 1,
+                    Name = "Lex",
+                    Telephone = "0883368643",
+                    Email = "lex@yahoo.com",
+                    WebSiteURL = "www.lex-company.com"
+                     },
+                new Manufacturer()
+                    {
+                    Id = 2,
+                    Name = "Krasi",
+                    Telephone = "0883368999",
+                    Email = "krasi-white-magic@abv.bg",
+                    WebSiteURL = "www.krasi-plant.com"
+                     },
+            };
+            Product product = new Product
+            {
+                Name = "Prah za prane LEX",
+                Description = "Hitriqt nachin za prane Lex, Rex i t.n...",
+                PriceBGN = 12.43m,
+                WeightKgs = 2,
+                ProductType = ProductType.CleaningSupplies,
+                Manufacturer = proizvoditeli[0],
+                ExpirationDate = DateTime.ParseExact("02-10/2024", "dd-MM/yyyy", CultureInfo.InvariantCulture)
+            };
+        }
+    }
+
+    public class Product
+    {
+        public decimal PriceBGN { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public double WeightKgs { get; set; }
+        public ProductType ProductType { get; set; }
+        public string Description { get; set; }
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public Manufacturer Manufacturer { get; set; }
+    }
+    public enum ProductType
+    {
+        Food, BeverageAlcohol, BeverageNonAlcohol, CleaningSupplies, Clothing
+    }
+
+
+
+
+    public class Manufacturer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Asen";
+        public string Telephone { get; set; }
+        public string WebSiteURL { get; set; }
+        public string Email { get; set; }
+
+        public Manufacturer()
+        {
+            Name = "Genadi";
+        }
+
+        public Manufacturer(string name, string phoneNumber)
+        {
+            Name = name;
+            Telephone = phoneNumber;
+        }
+
+        public Manufacturer(string name, string telephone, string webSiteURL, string email)
+        {
+            Name = name;
+            Telephone = telephone;
+            WebSiteURL = webSiteURL;
+            Email = email;
+        }
+
+        public void ChangeName()
+        {
+            Name = "Mitko";
+        }
+
+
+    }
+}
--- OOP/OOP 11A/Enumerations/Program.cs
+++ OOP/OOP 11A/Enumerations/Program.cs
@@ -1,10 +1,99 @@
-﻿namespace Enumerations
+﻿using System.Reflection.PortableExecutable;
+
+namespace Enumerations
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            List<Car> cars = new List<Car>();
+            var myCar = new Car()
+            {
+                YearOfMake = 1998,
+                Mileage = 80085,
+                Manufacturer = CarManufacturer.Mercedess, //1variant da go izberem
+                Model = "Celica"
+            };
+
+            var myCar2 = new Car()
+            {
+                YearOfMake = 2003,
+                Mileage = 60084,
+                Manufacturer = Enum.Parse<CarManufacturer>("Toyota"), //ot string
+                Model = "Celica"
+            };
+            cars.Add(myCar);
+            cars.Add(myCar2);
+
+            cars.Add(new Car()
+            {
+                YearOfMake = 2007,
+                Mileage = 50084,
+                Manufacturer = (CarManufacturer)13,// 3ti variant kastvane ot chislo
+                Model = "Corrola"
+            });
+
+            cars.Add(new Car()
+            {
+                YearOfMake = 2023,
+                Mileage = 0,
+                Manufacturer = CarManufacturer.Ford,// 3ti variant kastvane ot chislo
+                Model = "Mac-E"
+            });
+
+
+            foreach (Car car in cars)
+            {
+                if (car.Manufacturer == CarManufacturer.Toyota)
+                {
+                    Console.WriteLine("Imame toyota!!!");
+                }
+                switch (car.Manufacturer)
+                {
+                    case CarManufacturer.Ford:
+                        {
+                            Console.WriteLine("Imame Ford!!!");
+                            break;
+                        }
+                    case CarManufacturer.Opel:
+                        {
+                            Console.WriteLine("Imame Opel!!!");
+                            break;
+                        }
+                    case CarManufacturer.Mercedess:
+                        break;
+                    case CarManufacturer.Toyota:
+                        break;  
+                    default:
+                        break;
+                }
+            }
         }
     }
+    public class Car
+    {
+        private int yearOfMake;
+        public int Mileage { get; set; }
+        public string Model { get; set; }
+
+        public int YearOfMake
+        {
+            get { return yearOfMake; }
+            set { yearOfMake = value; }
+        }
+
+        public CarManufacturer Manufacturer { get; set; }
+    }
+
+
+    public enum CarManufacturer
+    {
+        Ford = 10,
+        Opel = 11,
+        Mercedess = 12,
+        Toyota = 13,
+        BMW = 9
+    }
+
+
 }
