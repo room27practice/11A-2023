edfd8edf58173573ff4d2c8a70060b105e451ed2
a <b>
2023-10-19 11:41:41 +0300

    add

--- BD/Relations Demo.sql
+++ BD/Relations Demo.sql
@@ -20,6 +20,7 @@ Description nvarchar(128),
 GO
 CREATE TABLE Shops (
 Id int PRIMARY KEY IDENTITY,
+Name nvarchar(16) NOT NULL,
 StartHour int NOT NULL,
 EndHour int NOT NULL,
 AddressId int NOT NULL 
@@ -48,3 +49,54 @@ REFERENCES Shops(Id),
 CONSTRAINT PK_PeopleShops PRIMARY KEY(PersonId,ShopId)
 )
 
+INSERT INTO Addresses(TownName,StreetName,StreetNumber,Description) VALUES
+('Shumen','Cherna gora',12,'Bqlata prust'),
+('Varna','Glarus',7,'Morskata Gradina'),
+('Shumen','Trakia',9,'Krugovoto na kauflanda')
+
+INSERT INTO People(EGN,Name,Age,Proffession,Email) VALUES
+('8743','Purvan Goshkov',35,N'Фудболист','purvanG@yahoo.com'),
+('9753','Vtorislav Goshkov',76,'Фудболист','gosholosho@abv.bg'),
+('2211','Tretomir Chetvurtoslavov',76,'Krupie','3tomir-gmail.com'),
+('1212','Petur Shestomirov',26,'Magaziner','5etur@abv.bg'),
+('H43S','Shestoslava Sedmomirova',17,'Dilar na Kozmetika','6i6arka@dir.bg')
+
+INSERT INTO Purposes(Name,Description) VALUES
+('Bakladjiinica','Mnogo zahar mnogo kalorii'),
+('Kafe','Ima masichki i se piqt kafeta moje i sok4eta'),
+('Hranitelen','Prodava se i sirene!'),
+('Za jivotni','Samo malki jivotni kravi ne!')
+
+INSERT INTO Shops(StartHour,EndHour,AddressId,PurposeId,Name) VALUES
+(8,20,1,7,'ZooMag 75 Eood'),
+(7,22,3,6,'Mambo'),
+(10,23,2,5,'Generala'),
+(6,16,1,4,'Golqmata baklava')
+
+INSERT INTO PeopleShops(ShopId,PersonId) VALUES
+(2,'H43S'),
+(4,'H43S'),
+(4,'1212'),
+(3,'9753'),
+(3,'8743')
+
+--ALTER TABLE Shops 
+--ADD Name VARCHAR(16) NOT NULL
+
+SELECT * FROM Addresses
+SELECT * FROM Purposes
+SELECT * FROM People
+SELECT * FROM Shops
+SELECT * FROM PeopleShops
+
+
+SELECT p.Name AS FullName,s.Name as Visited,pur.Description FROM People as p
+JOIN PeopleShops as ps
+ON p.EGN=ps.PersonId
+JOIN Shops as s
+ON ps.ShopId=s.Id
+JOIN Addresses AS a
+ON a.Id=s.AddressId
+JOIN Purposes as pur
+ON pur.Id=s.PurposeId
+WHERE Name LIKE '%ova'
