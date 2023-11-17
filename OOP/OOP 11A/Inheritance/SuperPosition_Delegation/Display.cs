808127e7b01011bfe88a715843ed86abebf48f7d
a <b>
2023-11-17 09:39:01 +0200

    added SuperPositionDemo

---
+++ OOP/OOP 11A/Inheritance/SuperPosition_Delegation/Display.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperPosition_Delegation
+{
+    public class Display
+    {
+        private int[] brightnessRange = new int[] { 0, 10 };
+        private int brightnessLevel = 5;
+        public double SizeInches { get; set; }
+        public int HeightResolution { get; set; } = 1080;
+        public int WidthResolution { get; set; } = 1920;
+        public TypeOfCover TypeOfCover { get; set; }
+        public int IncreaseBrigthness()
+        {
+            if (brightnessLevel < brightnessRange[1])
+            {
+                brightnessLevel++;
+            }
+            return brightnessLevel;
+        }
+        public int DecraseBrigthness()
+        {
+            if (brightnessLevel > brightnessRange[0])
+            {
+                brightnessLevel--;
+            }
+            return brightnessLevel;
+        }
+    }
+    public enum TypeOfCover
+    {
+        Glossy, Matt
+    }
+
+    public class SmartPhone
+    {
+        private Display display;
+        public SmartPhone(string model)
+        {
+            Model = model;
+            display = new Display();
+        }
+
+        public SmartPhone(string model, Display display) : this(model)
+        {
+            display = display;
+        }
+
+        public string Model { get; set; }
+
+        //Delegation
+        public void MakeBrighter()
+        {
+            int currentBrightnessLevel = display.IncreaseBrigthness();
+            Console.WriteLine($"Brightness was set to {currentBrightnessLevel}");
+
+
+        }
+
+
+    }
+
+
+}
