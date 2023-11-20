715ac2b4aee74bb96bd1359d56f0ce42a202a237
a <b>
2023-11-20 11:26:23 +0200

    add

--- OOP/OOP 11A/InheritanceExercise/Program.cs
+++ OOP/OOP 11A/InheritanceExercise/Program.cs
@@ -6,10 +6,12 @@ namespace IneritanceExercise
         public static void Main()
         {
             Console.WriteLine("Hello 11A");
-            Furniture furniture=new Furniture("Biufet",234.54m);
-            Bed bed1 = new Bed("Shiroko Leglo", 199.99m, 67, 45, 90, 200, BedSpec.Nails);
-
+            Furniture furniture = new Furniture("Biufet", 234.54m);
+            Bed bed1 = new Bed("Shiroko Leglo", 199.99m, 67, 45, 90, 200, BedSpec.TopMatress);
             Chair ch1 = new Chair("Vurtiasht Ofisen", 99.99m, 50);
+
+            Console.WriteLine(bed1.UsersCount);
+           
         }
     }
 }
